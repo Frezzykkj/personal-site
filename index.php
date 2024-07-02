13869cd74f000c63960a40e4c25f8286c099e5e2
Jão <joaorocha130709@gmail.com>
2024-07-02 11:46:38 -0300

    recomeçando area de resgistro e login

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="pt-br"> 
+
+<head>
+    <meta charset="utf-8">
+    <title>Personal Site</title>
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    <form method="POST" action="processa.php">
+        <input type="email">
+        <input type="password">
+        <input type="password">
+    </form>
+</body>
+
+</html>
