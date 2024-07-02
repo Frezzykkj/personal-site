f870c06762a3addac566846be562dae45f3899be
Jão <joaorocha130709@gmail.com>
2024-07-01 22:29:04 -0300

    banco de dados

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-
-    if(isset($_POST['submit']))
-    {
-        print_r($_POST['nome']);
-        print_r($_POST['email']);
-        print_r($_POST['password']);
-    }
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
