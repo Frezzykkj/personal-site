2a82a451d8339fe81cd81ef792f28cb5640773d1
Jão <joaorocha130709@gmail.com>
2024-07-04 18:45:42 -0300

    criando a tela inicial

--- index.php
+++ index.php
@@ -7,8 +7,18 @@
     }
 ?>
 
-SEJA BEM VINDO
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./Styles/home_style.css"> 
+</head>
+<body>
+    <form action="logout.php" method="post">
+        <button type="submit">Logout</button>
+    </form>
+</body>
+</html>
 
-<form action="logout.php" method="post">
-    <button type="submit">Logout</button>
-</form>
